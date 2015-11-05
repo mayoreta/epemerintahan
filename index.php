b4eb77e0462435c25f60f0f7981437d01789a5c0
mayoreta <lovianamayoreta@gmail.com>
2015-11-05 22:21:00 +0700

    Versi 1.0

---
+++ index.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ePemerintahan</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+<div class="bhead"></div>
+
+<div class="ads">
+    <div class="left">
+        <div class="box"><p class="title">IKLAN ATAS KIRI</p></div>
+    </div>
+    <div class="right">
+        <div class="box"><p class="title">IKLAN ATAS KANAN</p></div>
+    </div>
+</div>
+
+<div class="cont">
+    <div class="testi">
+        <div class="video"><img class="youtube" src="assets/img/youtube.png"></div>
+        <div><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</a></div>
+    </div>
+    <div class="testi">
+        <div class="video"><img class="youtube" src="assets/img/youtube.png"></div>
+        <div><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</a></div>
+    </div>
+    <div class="testi">
+        <div class="video"><img class="youtube" src="assets/img/youtube.png"></div>
+        <div><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</a></div>
+    </div>
+</div>
+
+<div class="navigasi">
+
+</div>
+
+<div class="portal">
+    <div class="hportal">
+        <div class="agenda">
+            <img class="logo" src="assets/img/portal.agenda-nusantara_white.png">
+        </div>
+    </div>
+    <div class="hportal">
+        <div class="nusantara">
+            <img class="logo" src="assets/img/portal.nusantara-updates_white.png">
+        </div>
+    </div>
+    <div class="hportal">
+        <div class="belanja">
+            <img class="logo" src="assets/img/portal.belanja-nusantara_white.png">
+        </div>
+    </div>
+    <div class="hportal">
+        <div class="komunitas">
+            <img class="logo" src="assets/img/portal.community-channel.png">
+        </div>
+    </div>
+</div>
+
+<div class="panel-footer">
+    <div class="footer-left">
+        <div class="group1">
+            <a href="#"><img class="icon" src="assets/img/portal.epemerintahan_white.png"></a>
+        </div>
+        <div class="group1">
+            <a href="#"><img class="icon" src="assets/img/portal.belanja-nusantara_white.png"></a>
+        </div>
+        <div class="group1">
+            <a href="#"><img class="icon" src="assets/img/portal.agenda-nusantara_white.png"></a>
+        </div>
+        <div class="group1">
+            <a href="#"><img class="icon" src="assets/img/portal.nusantara-updates_white.png"></a>
+        </div>
+        <div class="group1">
+            <a href="#"><img class="icon" src="assets/img/portal.community-channel_white.png"></a>
+        </div>
+    </div>
+    <div class="footer-right">
+        <div class="group3">
+            <a class="about" href="#">About Us</a>
+        </div>
+        <div class="group2">
+            <a href="#"><img class="icon" src="assets/img/tw.png"></a>
+        </div>
+        <div class="group2">
+            <a href="#"><img class="icon" src="assets/img/fb.jpg"></a>
+        </div>
+        <div class="group2">
+            <a href="#"><img class="icon" src="assets/img/rss.png"></a>
+        </div>
+    </div>
+</div>
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
